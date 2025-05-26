8ec906f64e676b0044f47e0ee7eff0368573979d
gitgudman <michaelquach604@gmail.com>
2025-05-26 07:26:31 -0700

    5th commit

--- play.js
+++ play.js
@@ -11,22 +11,7 @@ const game = new Game(new UserInterface(), new RemoteInterface())
 game.start()
 
 const { connect } = require("./client.js");
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
+const { setupInput } = require("./input");
 
 console.log("Connecting 1,2,3 ...");
 connect();
