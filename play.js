0b435e168cefd253a911b10b297594c4fe6eccae
gitgudman <michaelquach604@gmail.com>
2025-05-26 05:49:34 -0700

    first commit

--- play.js
+++ play.js
@@ -9,3 +9,32 @@ const game = new Game(new UserInterface(), new RemoteInterface())
 
 // Begin game
 game.start()
+
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // Replace with actual IP if different
+    port: 50541        // Replace with actual port if different
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // Event: successful connection
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+  });
+
+  // Event: incoming data from server
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
